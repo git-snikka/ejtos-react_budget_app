254847712d0e486287cd0095cc51a2f71188d2de
snikka <you@example.com>
2023-06-20 20:15:26 -0400

    6.20 changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
-import Budget from './components/Budget';
 
-// Add code to import the other components here under
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import ExpenseItem from './components/ExpenseItem';
+import Remaining from './components/Remaining';
 
 
 import { AppProvider } from './context/AppContext';
@@ -16,17 +19,35 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-
+                    // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         {/* Add Remaining component here under */}        
-
+                    // Budget component
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         {/* Add ExpenseTotal component here under */}        
-                       
+                    // Budget component
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         {/* Add ExpenseList component here under */}         
-
+                    // Budget component
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                         {/* Add ExpenseItem component here under */}        
-
+                    // Budget component
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
                         {/* Add AllocationForm component here under */}        
-
+                    // Budget component
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
